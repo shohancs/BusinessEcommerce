c723150788c63d0c13f44df2e7d707fe6bbb0f09
shohanurrahman1 <shohanurrahmanshohan.cs@gmail.com>
2025-03-20 03:10:07 +0600

    products page side bar customization

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -1,183 +1,127 @@
 <div class="col-sm-3 collection-filter">
-                        <!-- side-bar colleps block stat -->
-                        <div class="collection-filter-block">
-                            <!-- brand filter start -->
-                            <div class="collection-mobile-back"><span class="filter-back"><i class="fa fa-angle-left"
-                                        aria-hidden="true"></i> back</span></div>
-                            <div class="collection-collapse-block open">
-                                <h3 class="collapse-block-title">brand</h3>
-                                <div class="collection-collapse-block-content">
-                                    <div class="collection-brand-filter">
-                                        <div class="form-check collection-filter-checkbox">
-                                            <input type="checkbox" class="form-check-input" id="zara">
-                                            <label class="form-check-label" for="zara">zara</label>
-                                        </div>
-                                        <div class="form-check collection-filter-checkbox">
-                                            <input type="checkbox" class="form-check-input" id="vera-moda">
-                                            <label class="form-check-label" for="vera-moda">vera-moda</label>
-                                        </div>
-                                        <div class="form-check collection-filter-checkbox">
-                                            <input type="checkbox" class="form-check-input" id="forever-21">
-                                            <label class="form-check-label" for="forever-21">forever-21</label>
-                                        </div>
-                                        <div class="form-check collection-filter-checkbox">
-                                            <input type="checkbox" class="form-check-input" id="roadster">
-                                            <label class="form-check-label" for="roadster">roadster</label>
-                                        </div>
-                                        <div class="form-check collection-filter-checkbox">
-                                            <input type="checkbox" class="form-check-input" id="only">
-                                            <label class="form-check-label" for="only">only</label>
-                                        </div>
-                                    </div>
+
+    <!-- side-bar colleps block stat -->
+    <div class="collection-filter-block">
+
+        <!-- brand filter start -->
+        <div class="collection-mobile-back"><span class="filter-back"><i class="fa fa-angle-left"
+                    aria-hidden="true"></i> back</span></div>
+        <div class="collection-collapse-block open">
+            <h3 class="collapse-block-title">Category</h3>
+            <div class="collection-collapse-block-content">
+                <div class="collection-brand-filter pt-3">
+
+                    <?php  
+                        if ( isset($_GET['gen']) ) {
+                            $cateId = $_GET['gen'];
+
+                            $sql = "SELECT sc.*, c.name AS category_name FROM subcategory sc JOIN category c ON sc.cat_id = c.id WHERE sc.status = 1 AND sc.cat_id = '$cateId' ORDER BY sc.id ASC";
+                            $query = mysqli_query($db, $sql);
+
+                            while ($row = mysqli_fetch_assoc($query)) {
+                                $subid      = $row['id'];
+                                $name       = $row['name'];
+
+                                ?>
+                                <div class="d-flex align-items-center">
+                                    <i class="fa-solid fa-caret-right"></i>
+                                    <h6> <a href="" class="form-check-label ps-2 text-dark fs-6"><?php echo $name; ?></a></h6>
                                 </div>
-                            </div>
-                            <!-- color filter start here -->
-                            <div class="collection-collapse-block open">
-                                <h3 class="collapse-block-title">colors</h3>
-                                <div class="collection-collapse-block-content">
-                                    <div class="color-selector">
-                                        <ul>
-                                            <li class="color-1 active"></li>
-                                            <li class="color-2"></li>
-                                            <li class="color-3"></li>
-                                            <li class="color-4"></li>
-                                            <li class="color-5"></li>
-                                            <li class="color-6"></li>
-                                            <li class="color-7"></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- size filter start here -->
-                            <div class="collection-collapse-block border-0 open">
-                                <h3 class="collapse-block-title">size</h3>
-                                <div class="collection-collapse-block-content">
-                                    <div class="collection-brand-filter">
-                                        <div class="form-check collection-filter-checkbox">
-                                            <input type="checkbox" class="form-check-input" id="hundred">
-                                            <label class="form-check-label" for="hundred">s</label>
-                                        </div>
-                                        <div class="form-check collection-filter-checkbox">
-                                            <input type="checkbox" class="form-check-input" id="twohundred">
-                                            <label class="form-check-label" for="twohundred">m</label>
-                                        </div>
-                                        <div class="form-check collection-filter-checkbox">
-                                            <input type="checkbox" class="form-check-input" id="threehundred">
-                                            <label class="form-check-label" for="threehundred">l</label>
-                                        </div>
-                                        <div class="form-check collection-filter-checkbox">
-                                            <input type="checkbox" class="form-check-input" id="fourhundred">
-                                            <label class="form-check-label" for="fourhundred">xl</label>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- price filter start here -->
-                            <div class="collection-collapse-block border-0 open">
-                                <h3 class="collapse-block-title">price</h3>
-                                <div class="collection-collapse-block-content">
-                                    <div class="wrapper mt-3">
-                                        <div class="range-slider">
-                                            <input type="text" class="js-range-slider" value="" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- silde-bar colleps block end here -->
-                        <!-- side-bar single product slider start -->
-                        <div class="theme-card">
-                            <h5 class="title-border">new product</h5>
-                            <div class="offer-slider slide-1">
-                                <div>
-                                    <div class="media">
-                                        <a href=""><img class="img-fluid blur-up lazyload"
-                                                src="assets/images/fashion/pro/1.jpg" alt=""></a>
-                                        <div class="media-body align-self-center">
-                                            <div class="rating"><i class="fa fa-star"></i> <i class="fa fa-star"></i>
-                                                <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i
-                                                    class="fa fa-star"></i>
-                                            </div><a href="product-page(no-sidebar).html">
-                                                <h6>Green Printed Dresses</h6>
-                                            </a>
-                                            <h4>30.00</h4>
-                                        </div>
-                                    </div>
-                                    <div class="media">
-                                        <a href=""><img class="img-fluid blur-up lazyload"
-                                                src="assets/images/fashion/pro/011.jpg" alt=""></a>
-                                        <div class="media-body align-self-center">
-                                            <div class="rating"><i class="fa fa-star"></i> <i class="fa fa-star"></i>
-                                                <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i
-                                                    class="fa fa-star"></i>
-                                            </div><a href="product-page(no-sidebar).html">
-                                                <h6>Pink Printed Dresses</h6>
-                                            </a>
-                                            <h4>$35.00</h4>
-                                        </div>
-                                    </div>
-                                    <div class="media">
-                                        <a href=""><img class="img-fluid blur-up lazyload"
-                                                src="assets/images/fashion/pro/16.jpg" alt=""></a>
-                                        <div class="media-body align-self-center">
-                                            <div class="rating"><i class="fa fa-star"></i> <i class="fa fa-star"></i>
-                                                <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i
-                                                    class="fa fa-star"></i>
-                                            </div><a href="product-page(no-sidebar).html">
-                                                <h6>Solid Green Dresses</h6>
-                                            </a>
-                                            <h4>$33.00</h4>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div class="media">
-                                        <a href=""><img class="img-fluid blur-up lazyload"
-                                                src="assets/images/fashion/pro/001.jpg" alt=""></a>
-                                        <div class="media-body align-self-center">
-                                            <div class="rating"><i class="fa fa-star"></i> <i class="fa fa-star"></i>
-                                                <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i
-                                                    class="fa fa-star"></i>
-                                            </div><a href="product-page(no-sidebar).html">
-                                                <h6>black White Printed Dresses</h6>
-                                            </a>
-                                            <h4>$32.00</h4>
-                                        </div>
-                                    </div>
-                                    <div class="media">
-                                        <a href=""><img class="img-fluid blur-up lazyload"
-                                                src="assets/images/fashion/pro/4.jpg" alt=""></a>
-                                        <div class="media-body align-self-center">
-                                            <div class="rating"><i class="fa fa-star"></i> <i class="fa fa-star"></i>
-                                                <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i
-                                                    class="fa fa-star"></i>
-                                            </div><a href="product-page(no-sidebar).html">
-                                                <h6>Dotted Black Dresses</h6>
-                                            </a>
-                                            <h4>$38.00</h4>
-                                        </div>
-                                    </div>
-                                    <div class="media">
-                                        <a href=""><img class="img-fluid blur-up lazyload"
-                                                src="assets/images/fashion/pro/19.jpg" alt=""></a>
-                                        <div class="media-body align-self-center">
-                                            <div class="rating"><i class="fa fa-star"></i> <i class="fa fa-star"></i>
-                                                <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i
-                                                    class="fa fa-star"></i>
-                                            </div><a href="product-page(no-sidebar).html">
-                                                <h6>Blue Printed Dresses</h6>
-                                            </a>
-                                            <h4>$36.00</h4>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- side-bar single product slider end -->
-                        <!-- side-bar banner start here -->
-                        <div class="collection-sidebar-banner">
-                            <a href="#"><img src="assets/images/side-banner.png" class="img-fluid blur-up lazyload"
-                                    alt=""></a>
-                        </div>
-                        <!-- side-bar banner end here -->
+                                <?php
+                            }
+                        }
+                    ?>
+
+                    
+
+                </div>
+            </div>
+        </div>
+
+        <!-- price filter start here -->
+        <div class="collection-collapse-block border-0 open">
+            <h3 class="collapse-block-title">price</h3>
+            <div class="collection-collapse-block-content">
+                <div class="wrapper mt-3">
+                    <div class="range-slider">
+                        <input type="text" class="js-range-slider" value="" />
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- silde-bar colleps block end here -->
+
+    <!-- side-bar single product slider start -->
+    <div class="theme-card">
+        <h5 class="title-border">new product</h5>
+        <div class="offer-slider slide-1">
+            <?php  
+                $sql = "SELECT * FROM products WHERE cat_id='$cateId' ORDER BY id DESC";
+                $query = mysqli_query($db, $sql);
+
+                while($row = mysqli_fetch_assoc($query)) {
+                    $id             = $row['id'];
+                    $prd_code       = $row['prd_code'];
+                    $name           = $row['name'];
+                    $price          = $row['price'];
+                    $dsc_price      = $row['dsc_price'];
+                    $img1           = $row['img1'];
+                    ?>
+                    <div>
+                        <div class="media">
+
+                            <a href="details.php?did=<?php echo $id; ?>"> <?php  
+                                if ( !empty($img1) ) {
+                                    echo '<img src="admin/assets/images/products/' . $img1 . '" alt="" class="img-fluid blur-up lazyload">';
+                                }
+                                else { ?>
+                                    <img
+                                src="assets/images/pro3/36.jpg"
+                                 alt="">
+                               <?php }
+                            ?></a>
+                            <div class="media-body align-self-center">
+                                <span class="text-secondary"><?php echo $prd_code; ?></span>
+                                <a href="details.php?did=<?php echo $id; ?>">
+                                    <h6 class="text-dark mb-0"><?php echo $name; ?></h6>
+                                </a>
+                                <h4>BDT <?php echo $dsc_price; ?><br> <span><del> ৳ <?php echo $price; ?></del></span></h4>
+                            </div>
+                        </div>
+                    </div>
+                    <?php
+                }
+
+            ?>
+        </div>
+    </div>
+    <!-- side-bar single product slider end -->
+    <!-- side-bar banner start here -->
+    <div class="collection-sidebar-banner">
+        <?php  
+            $sql = "SELECT * FROM products WHERE cat_id='$cateId' ORDER BY id DESC LIMIT 1";
+            $query = mysqli_query($db, $sql);
+
+            while($row = mysqli_fetch_assoc($query)){
+                $id   = $row['id'];
+                $img3   = $row['img3'];
+                ?>
+                <a href="details.php?did=<?php echo $id; ?>">
+                    <?php  
+                        if ( !empty($img3) ) {
+                            echo '<img src="admin/assets/images/products/' . $img3 . '" alt="" class="img-fluid blur-up lazyload">';
+                        }
+                        else { ?>
+                            <img src="assets/images/pro3/36.jpg" alt="">
+                        <?php }
+                    ?>
+                </a>
+                <?php
+            }
+        ?>
+
+                
+    </div>
+    <!-- side-bar banner end here -->
+</div>
